e90d6600d72957b01020a9a6f35caea29c3a0b6c
Francis Fernandez <francisfernandez0679@gmail.com>
2025-07-11 22:45:13 +0800

    Removed router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <router-view></router-view>
+  <div class="flex flex-col items-center justify-center w-screen h-screen gap-10 bg-slate-900">
+    <h1 class="text-2xl italic font-bold text-white drop-shadow-lg">Hello World!</h1>
+    <a href="https://github.com/Ax1a" target="_blank" class="px-3 py-2 text-sm text-teal-200 duration-200 bg-teal-700 rounded-lg hover:bg-teal-800">
+      Template by <span class="italic font-bold">Axia</span>
+    </a>
   </div>
 </template>
 <script></script>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import { Icon } from '@iconify/vue'
 import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import('@/views/Home.vue')
-    }
-  ]
-})
 const app = createApp(App)
 const pinia = createPinia()
 
 app.component('Icon', Icon)
-app.use(router)
 app.use(pinia)
 app.mount('#app')
